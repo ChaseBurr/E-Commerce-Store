e9fdb5dbc31b0c8693161dbbd4f43cf5361344f8
Chase <chase@chaseburr.com>
2021-01-25 23:31:24 -0700

    added remove, update, and empty cart call back components

--- src/App.js
+++ src/App.js
@@ -20,8 +20,23 @@ function App() {
    };
 
    const handleAddToCart = async (productId, quantity) => {
-      const item = await commerce.cart.add(productId, quantity);
-      setCart(item.cart);
+      const { cart } = await commerce.cart.add(productId, quantity);
+      setCart(cart);
+   };
+
+   const handleUpdateCardQty = async (productId, quantity) => {
+      const { cart } = await commerce.cart.update(productId, { quantity });
+      setCart(cart);
+   };
+
+   const handleRemoveFromCart = async (productId) => {
+      const { cart } = await commerce.cart.remove(productId);
+      setCart(cart);
+   };
+
+   const handleEmptyCart = async () => {
+      const { cart } = await commerce.cart.empty();
+      setCart(cart);
    };
 
    useEffect(() => {
@@ -38,7 +53,12 @@ function App() {
                   <Products products={products} onAddToCart={handleAddToCart} />
                </Route>
                <Route exact path="/cart">
-                  <Cart cart={cart} />
+                  <Cart
+                     cart={cart}
+                     handleUpdateCardQty={handleUpdateCardQty}
+                     handleRemoveFromCart={handleRemoveFromCart}
+                     handleEmptyCart={handleEmptyCart}
+                  />
                </Route>
             </Switch>
          </Router>
