17b4230538a9a0e00a25b8e78b9e9c59736a9414
Chase <chase@chaseburr.com>
2021-01-25 19:12:21 -0700

    added cart imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 // Export components
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Products } from "./Products/Products";
+export { default as Cart } from "./Cart/Cart";
