97edecb29cbcb7d145a9ebaeaaa527dadcb672c4
Chase <chase@chaseburr.com>
2021-01-25 23:32:47 -0700

    added button call back functions

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
 import CartItem from "./CartItem/CartItem";
 
 import useStyle from "./style";
 
-function Cart({ cart }) {
+function Cart({
+   cart,
+   handleUpdateCardQty,
+   handleRemoveFromCart,
+   handleEmptyCart,
+}) {
    const classes = useStyle();
 
    const EmptyCart = () => (
       <Typography variant="subtitle1">
-         You have no items in your cart, start adding some!
+         You have no items in your cart,
+         <Link to="/" className={classes.link}>
+            start adding some
+         </Link>
+         !
       </Typography>
    );
 
@@ -18,7 +28,11 @@ function Cart({ cart }) {
          <Grid container spacing={3}>
             {cart.line_items.map((item) => (
                <Grid item xs={12} sm={4} key={item.id}>
-                  <CartItem item={item} />
+                  <CartItem
+                     item={item}
+                     handleUpdateCardQty={handleUpdateCardQty}
+                     handleRemoveFromCart={handleRemoveFromCart}
+                  />
                </Grid>
             ))}
          </Grid>
@@ -32,6 +46,7 @@ function Cart({ cart }) {
                      type="button"
                      variant="contained"
                      color="secondary"
+                     onClick={handleEmptyCart}
                   >
                      Empty Cart
                   </Button>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import {
    AppBar,
    Toolbar,
@@ -16,11 +17,15 @@ import Logo from "./../../assets/Logo.svg";
 
 function Navbar({ totalItems }) {
    const classes = useStyles();
+   const location = useLocation();
+
    return (
       <>
          <AppBar position="fixed" className={classes.appBar} color="inherit">
             <Toolbar>
                <Typography
+                  component={Link}
+                  to="/"
                   variant="h6"
                   className={classes.title}
                   color="inherit"
@@ -34,13 +39,20 @@ function Navbar({ totalItems }) {
                   Commerce JS
                </Typography>
                <div className={classes.grow}></div>
-               <div className={classes.button}>
-                  <IconButton aria-label="Show Cart items" color="inherit">
-                     <Badge badgeContent={totalItems} color="secondary">
-                        <ShoppingCart />
-                     </Badge>
-                  </IconButton>
-               </div>
+               {location.pathname === "/" && (
+                  <div className={classes.button}>
+                     <IconButton
+                        component={Link}
+                        to="/cart"
+                        aria-label="Show Cart items"
+                        color="inherit"
+                     >
+                        <Badge badgeContent={totalItems} color="secondary">
+                           <ShoppingCart />
+                        </Badge>
+                     </IconButton>
+                  </div>
+               )}
             </Toolbar>
          </AppBar>
       </>
