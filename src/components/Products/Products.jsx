e51ba0b3875a6ef645a6988ea294ba40e22cd46e
Chase <chase@chaseburr.com>
2021-01-25 18:41:00 -0700

    updated product card to support api fetch

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -7,15 +7,15 @@ import useStyles from "./styles";
 import Product from "./Product/Product";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default ({ products }) => {
+export default ({ products, onAddToCart }) => {
    const classes = useStyles();
    return (
       <main className={classes.content}>
          <div className={classes.toolbar} />
          <Grid container justify="center" spacing={4}>
             {products.map((product) => (
-               <Grid key={product.key} item xs={12} sm={6} md={4} lg={3}>
-                  <Product product={product} />
+               <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+                  <Product product={product} onAddToCart={onAddToCart} />
                </Grid>
             ))}
          </Grid>
